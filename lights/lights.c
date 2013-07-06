1cb629e19189dba5aee598c26f812bee4f288c84
dh-harald <github@criminal.hu>
2013-07-06 02:19:07 +0200

    Dummy BLN
    Lights from aries tree...
    
    Using button backlights as notifications led
    ... Testing battery consumption

--- lights/lights.c
+++ lights/lights.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2013 Marcin Chojnacki
+ * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2011 <kang@insecure.ws>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,33 +16,32 @@
  */
 
 #define LOG_TAG "lights"
-
 #include <cutils/log.h>
-
 #include <stdint.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
-
+#include <pthread.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-
 #include <hardware/lights.h>
-#include <hardware/hardware.h>
 
+static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-/** Paths to light files **/
-char const *const BACKLIGHT_FILE = "/sys/class/backlight/panel/brightness";
-char const *const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
+char const *const LCD_FILE = "/sys/class/backlight/panel/brightness";
+char const *const LED_FILE = "/sys/class/leds/button-backlight/brightness";
 
-/** Write integer to file **/
 static int write_int(char const *path, int value)
 {
 	int fd;
-	static int already_warned = -1;
+	static int already_warned;
+
+	already_warned = 0;
+
+	ALOGV("write_int: path %s, value %d", path, value);
 	fd = open(path, O_RDWR);
+
 	if (fd >= 0) {
 		char buffer[20];
 		int bytes = sprintf(buffer, "%d\n", value);
@@ -50,7 +49,7 @@ static int write_int(char const *path, int value)
 		close(fd);
 		return amt == -1 ? -errno : 0;
 	} else {
-		if (already_warned == -1) {
+		if (already_warned == 0) {
 			ALOGE("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
@@ -58,63 +57,68 @@ static int write_int(char const *path, int value)
 	}
 }
 
-/** Convert RGB to 0-255 integer **/
 static int rgb_to_brightness(struct light_state_t const *state)
 {
 	int color = state->color & 0x00ffffff;
-	return ((77 * ((color >> 16) & 0x00ff))
-		+ (150 * ((color >> 8) & 0x00ff)) +
-		(29 * (color & 0x00ff))) >> 8;
+
+	return ((77*((color>>16) & 0x00ff))
+		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
 }
 
-/** Is light already enabled? **/
-static int is_lit(struct light_state_t const* state)
+static int set_light_notifications(struct light_device_t* dev,
+			struct light_state_t const* state)
 {
-    return state->color & 0x00ffffff;
+	int brightness =  rgb_to_brightness(state);
+	int v = 0;
+	int ret = 0;
+	pthread_mutex_lock(&g_lock);
+
+	if (brightness+state->color == 0 || brightness > 100) {
+		if (state->color & 0x00ffffff)
+			v = 1;
+	} else
+		v = 0;
+
+	ALOGI("color %u fm %u status %u is lit %u brightness", state->color, state->flashMode, v, (state->color & 0x00ffffff), brightness);
+	ret = write_int(LED_FILE, v);
+	pthread_mutex_unlock(&g_lock);
+	return ret;
 }
 
-/** Set LCD backlight **/
-static int set_light_backlight(struct light_device_t *dev, struct light_state_t const *state)
+static int set_light_backlight(struct light_device_t *dev,
+			struct light_state_t const *state)
 {
-    pthread_mutex_lock(&g_lock);
-    int err = write_int(BACKLIGHT_FILE, rgb_to_brightness(state));
-    pthread_mutex_unlock(&g_lock);
-    return err;
+	int err = 0;
+	int brightness = rgb_to_brightness(state);
+
+	pthread_mutex_lock(&g_lock);
+	err = write_int(LCD_FILE, brightness);
+
+	pthread_mutex_unlock(&g_lock);
+	return err;
 }
 
-/** Set buttons backlight **/
-static int set_light_buttons(struct light_device_t* dev, struct light_state_t const* state)
-{
-    int err = 0;
-    pthread_mutex_lock (&g_lock);
-    if(is_lit(state))
-        err = write_int(BUTTON_FILE, 1);
-    else
-        err = write_int(BUTTON_FILE, 0);
-    pthread_mutex_unlock (&g_lock);
-    return err;
-}
-
-/** Close the lights device */
 static int close_lights(struct light_device_t *dev)
 {
+	ALOGV("close_light is called");
 	if (dev)
 		free(dev);
+
 	return 0;
 }
 
-/** Open a new instance of a lights device using name */
-static int open_lights(const struct hw_module_t *module, char const *name, struct hw_device_t **device)
+static int open_lights(const struct hw_module_t *module, char const *name,
+						struct hw_device_t **device)
 {
-	pthread_t lighting_poll_thread;
+	int (*set_light)(struct light_device_t *dev,
+		struct light_state_t const *state);
 
-	int (*set_light) (struct light_device_t *dev,
-			  struct light_state_t const *state);
+	ALOGV("open_lights: open with %s", name);
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
-	else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
+	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
+		set_light = set_light_notifications;
 	else
 		return -EINVAL;
 
@@ -134,20 +138,16 @@ static int open_lights(const struct hw_module_t *module, char const *name, struc
 	return 0;
 }
 
-/** Method list **/
-static struct hw_module_methods_t lights_methods =
-{
+static struct hw_module_methods_t lights_module_methods = {
 	.open =  open_lights,
 };
 
-/** The backlight module **/
-struct hw_module_t HAL_MODULE_INFO_SYM =
-{
+struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.tag = HARDWARE_MODULE_TAG,
 	.version_major = 1,
 	.version_minor = 0,
 	.id = LIGHTS_HARDWARE_MODULE_ID,
-	.name = "Montblanc lights module",
-	.author = "Marcin Chojnacki",
-	.methods = &lights_methods,
+	.name = "lights Module",
+	.author = "Google, Inc.",
+	.methods = &lights_module_methods,
 };
